fcf841fe6b1f852ea2ebc3fbc85958473dacfcdd
Joaquin <joaquindiaz66@gmail.com>
2024-02-21 14:28:27 +0100

    se han creado POST,DELETE,GET...

--- src/app/quienes-somos/quienes-somos.component.ts
+++ src/app/quienes-somos/quienes-somos.component.ts
@@ -1,23 +1,92 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PlaceHolder } from '../interfaces/place-holder';
+
 
 @Component({
   selector: 'app-quienes-somos',
   templateUrl: './quienes-somos.component.html',
-  styleUrl: './quienes-somos.component.css'
+  styleUrl: './quienes-somos.component.css',
 })
 export class QuienesSomosComponent {
-  public url:string = 'https:\\jsonplaceholder.typicode.com/posts/1'
+  public url: string = 'https:\\jsonplaceholder.typicode.com/posts';
   public resultadoPeticion: any;
+  public inputIdGet: string = '';
+  constructor(private http: HttpClient) {}
 
-  constructor (private http: HttpClient){
-    
+  ngOnInit() {
+    this.get();
   }
 
+  get() {
+    this.http.get(this.url).subscribe((data) => {
+      this.resultadoPeticion = data;
+    });
+  }
 
-  get(){
-    this.http.get(this.url)
-    .subscribe( data =>{this.resultadoPeticion = data})
+  get2() {
+    this.http
+      .get<PlaceHolder>(this.url + '/' + this.inputIdGet)
+      .subscribe((data) => {
+        this.resultadoPeticion = data;
+        console.log(this.resultadoPeticion.id);
+        console.log(this.resultadoPeticion.userId);
+        console.log(this.resultadoPeticion.title);
+        console.log(this.resultadoPeticion.body);
+      });
+  }
+
+  get3() {
+    this.http.get<PlaceHolder>(this.url + '/' + this.inputIdGet).subscribe({
+      next: (respuesta: PlaceHolder) => {
+        this.resultadoPeticion = respuesta
+        console.log(this.resultadoPeticion.id);
+        console.log(this.resultadoPeticion.userId);
+        console.log(this.resultadoPeticion.title);
+        console.log(this.resultadoPeticion.body);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  post(){
+    this.http.post(this.url , 
+    {
+      userId: 2,
+      id: 200,
+      title: 'Nuevo titulo de prueba',
+      body: 'Nuevo cuerpo de prueba'
+    }).subscribe(data => {this.resultadoPeticion = data})
+  }
+
+  put(){
+    this.http.put(
+      this.url + "/10",
+      {
+        userId: 2,
+       
+        title: 'Nuevo titulo de prueba',
+        body: 'Nuevo cuerpo de prueba'
+      }
+     ).subscribe(data => {this.resultadoPeticion = data})
+  }
+  patch(){
+    this.http.patch(
+      this.url + "/10",
+      {
+        userId: 2,
+       
+        title: 'Nuevo titulo de prueba',
+        body: 'Nuevo cuerpo de prueba'
+      }
+     ).subscribe(data => {this.resultadoPeticion = data})
+  }
+
+  delete(){
+    this.http.delete(this.url + '/10')
+    .subscribe(data => {this.resultadoPeticion = data})
   }
 
 }
